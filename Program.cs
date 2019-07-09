1b508aefa3b329d2492a9c1a19cde46d5b75f5c9
Jason Collum <jasoncollum@gmail.com>
2019-07-09 08:33:31 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+            };
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+
+                if (numbers.Contains(i))
+                {
+                    Console.WriteLine($"Numbers List contains {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"Numbers List does not contain {i}");
+                }
+            }
+        }
+    }
+}
